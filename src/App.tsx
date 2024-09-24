a216a5aafd9dc65b1f1c091ffd1d33857a8bb5b1
nricaurt1 <nricaurt1@cuc.edu.co>
2024-09-24 12:50:23 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import milogo from "../src/assets/img/ESCUDO_JUNIOR.svg.png"
+
+function App() {
+  return (
+    <>
+    <div>
+      <img src={milogo} alt="Logo de junior" />
+      <h1>¡¡ Hola Mi Gente !!</h1>
+      <p>Este es el taller03 de Esteban Suarez </p>
+    </div>
+    </>
+  )
+}
+
+export default App
